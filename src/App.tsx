643611ab1c44bff2ff14307615765f0ed983cd8d
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-02-20 21:24:14 +0800

    fix: fix merge conflict for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './Router'
+import Navbar from '../src/components/Navbar';
 
 function App() {
   return(
   <>
+  <Navbar />
   <h1>Love The Voiceless</h1>
  <RouterProvider router = {router} />;
   </>)
